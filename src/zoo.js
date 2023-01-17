e80cb235bc8ca3099414810f1096d77c06beb338
Eugenio <eugeniolopesarantesjunior@gmail.com>
2023-01-17 15:11:55 -0300

    development of data structures

--- src/zoo.js
+++ src/zoo.js
@@ -11,56 +11,204 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // insira seu código aqui
+function animalsByIds(...ids) {
+  const animals = ids.map(( id ) => {
+    return data.animals.find(( animal ) => animal.id === id);
+  })
+
+  return animals;
 }
 
 function animalsOlderThan(animal, age) {
-  // insira seu código aqui
+  const animalFound = data.animals.find(({name}) => name === animal);
+
+  return animalFound.residents.every((animal) => animal.age >= age);
 }
 
 function employeeByName(employeeName) {
-  // insira seu código aqui
+  if (!employeeName) return {};
+
+  return data.employees.find(({firstName, lastName}) => employeeName === firstName || employeeName === lastName);
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // insira seu código aqui
+function createEmployee(personalInfo, ...associatedWith) {
+  const [managers, responsibleFor] = associatedWith;
+
+  const employee = {
+    ...personalInfo,
+    ...managers,
+    ...responsibleFor,
+  }
+
+  return employee;
 }
 
 function isManager(id) {
-  // insira seu código aqui
+  return !!data.employees.find(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // insira seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const personalInfo = {id, firstName, lastName};
+  const associatedWith = { managers, responsibleFor }
+
+  const employee = createEmployee(personalInfo, associatedWith);
+
+  return data.employees.push(employee);
 }
 
-function animalCount(species) {
-  // insira seu código aqui
+function animalCount(specie) {
+  if ( specie ) return data.animals.find(({name}) => name === specie).residents.length;
+
+  return data.animals.reduce((acc, { name, residents }) => {
+    return {
+      ...acc,
+      [name]: residents.length
+    }
+  }, {});
 }
 
-function entryCalculator(entrants) {
-  // insira seu código aqui
+function entryCalculator(entrants = {}) {
+  return Object.entries(entrants).reduce((acc, [ageGroup, quantity]) =>{
+    return acc = acc + data.prices[ageGroup] * quantity;
+  }, 0);
 }
+
+const locationBase = {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+}
+
+const mapConfig = {
+  includeNames: (_, specie, ...residents) => {
+    return [{[specie]: residents.map(({ name }) => name)}];
+  },
+  sex: (sex, specie, ...residents) => {
+    return [{[specie]: residents.filter(({ name, sex: animalSex }) => animalSex === sex)
+      .map(({ name }) => name)}];
+  },
+  sorted: (sex, specie, ...residents) => {
+    if (sex) {
+      return [{[specie]: residents.filter(({ name, sex: animalSex }) => animalSex === sex)
+      .map(({ name }) => name).sort()}];
+    }
+
+    return [{[specie]: residents.map(({ name }) => name).sort()}];
+  },
+};
 
 function animalMap(options) {
-  // insira seu código aqui
+  const verifiedOptions = options ? Object.keys(options) : [];
+
+  const mapNames = !options || !options.includeNames;
+
+  return data.animals.reduce((acc, { location, name, residents }) => {
+    let animals;
+
+    verifiedOptions.forEach((option) => {
+      animals = mapConfig[option](options['sex'], name, ...residents);
+    });
+
+    return {
+      ...acc,
+      [location]: mapNames ? [...acc[location], name] : [...acc[location], ...animals],
+    }
+  },locationBase);
 }
 
+function convert24HourToAmPm(open, close) {
+  return [open % 12, close % 12];
+};
+
+function phraseBuilder(day, {open, close}) {
+  if(day === 'Monday') return 'CLOSED';
+
+  const [openHourConverted, closeHourConverted] = convert24HourToAmPm(open, close);
+
+  return `Open from ${openHourConverted}am until ${closeHourConverted}pm`;
+};
+
 function schedule(dayName) {
-  // insira seu código aqui
+  if (dayName) {
+    const [day, hour] = Object.entries(data.hours).find(([day]) => day === dayName);
+
+    return { [day]: phraseBuilder(day, hour) }
+  }
+
+  return Object.entries(data.hours).reduce((acc, [day, hours]) => {
+    return {
+      ...acc,
+      [day]: phraseBuilder(day, hours),
+    }
+  }, {});
 }
 
 function oldestFromFirstSpecies(id) {
-  // insira seu código aqui
+  const employee = data.employees.find((employee) => employee.id === id);
+
+  const [ specieId ] = employee.responsibleFor;
+
+  const animal = data.animals.find(({ id }) => id === specieId);
+
+  const { name, sex, age} = animal.residents.reduce((acc, resident) => {
+
+    if (resident.age > acc.age) acc = resident;
+
+    return acc;
+  });
+
+  return [ name, sex, age ];
 }
+
+function increasePercentageCalculate(price, percentage) {
+  const calculatedPrice = price + ((price * percentage) / 100);
+
+  const adjustToTwoDecimalCases = Math.round(calculatedPrice * 100) / 100;
+
+  return adjustToTwoDecimalCases;
+};
 
 function increasePrices(percentage) {
-  // insira seu código aqui
+  const prices = Object.entries(data.prices).reduce((acc, [ageGroup, price]) => {
+    return {
+      ...acc,
+      [ageGroup]: increasePercentageCalculate(price, percentage)
+    }
+  }, {});
+
+  data.prices = prices;
 }
 
+function searchResponsibleAnimals(...responsibleFor) {
+  return animalsByIds(...responsibleFor).map(({name}) => name);
+};
+
+function concatName (firstName, lastName) {
+  return `${firstName} ${lastName}`;
+};
+
 function employeeCoverage(idOrName) {
-  // insira seu código aqui
+  if (!idOrName) {
+    return data.employees.reduce((acc, {firstName, lastName, responsibleFor}) => {
+      const name = concatName(firstName, lastName);
+
+      return {
+        ...acc,
+        [name]: searchResponsibleAnimals(...responsibleFor),
+      }
+    }, {});
+  }
+
+  const employee = data.employees.find(({ id, firstName, lastName }) => id === idOrName ||
+   firstName === idOrName ||lastName === idOrName);
+
+  const name = concatName(employee.firstName, employee.lastName);
+
+  return {
+    [name]: searchResponsibleAnimals(...employee.responsibleFor)
+  }
+
 }
 
 module.exports = {
